677d7b8abf623830b9a11058917aeb3d330bb622
RobDWaller <rdwaller1984@googlemail.com>
2020-02-24 12:53:28 +0000

    Created build directives function to create multiple directive strings. Added associated tests.

--- src/csp_string.rs
+++ src/csp_string.rs
@@ -1,5 +1,10 @@
 use crate::csp_json;
 
+const DIRECTIVE_LIST: [&str; 2] = [
+    "script-src", 
+    "connect-src"
+]; 
+
 pub fn directive_line(directive: &str, csp: csp_json::CspJson) -> String {
     let mut directive_line: String = directive.to_string();
     directive_line.push_str(":");
@@ -11,6 +16,19 @@ pub fn directive_line(directive: &str, csp: csp_json::CspJson) -> String {
         }
     }
 
-    directive_line.push_str(";");
+    directive_line.push_str("; ");
     return directive_line;
+}
+
+pub fn build_directives(json: &str) -> String {
+    let mut directives: String = String::from("");
+
+    for directive in DIRECTIVE_LIST.iter() {
+        let option: Option<csp_json::CspJson> = csp_json::json_to_csp(json);
+        if !option.is_none() {
+            directives.push_str(self::directive_line(directive, option.unwrap()).as_str());
+        }
+    }
+    
+    return directives.trim().to_string();
 }
--- tests/csp_strings_test.rs
+++ tests/csp_strings_test.rs
@@ -28,6 +28,22 @@ mod csp_string_test {
 
         let connect_src: String = csp_string::directive_line("connect-src", json);
 
-        assert_eq!(connect_src, String::from("connect-src: *.example.com;"));
+        assert_eq!(connect_src, String::from("connect-src: *.example.com; "));
+    }
+
+    #[test]
+    fn test_build_directives() {
+        let json = r#"
+            {
+                "domains": [
+                    {"domain": "example.com", "directive": ["connect-src"]},
+                    {"domain": "test.com", "directive": ["connect-src", "script-src"]}
+                ]
+            }
+        "#;
+
+        let csp: String = csp_string::build_directives(json);
+
+        assert_eq!(csp, String::from("script-src: test.com; connect-src: example.com test.com;"));
     }
 }

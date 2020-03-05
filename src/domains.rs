a98dd73530ff7430a419c87843857874a5844ead
RobDWaller <rdwaller1984@googlemail.com>
2020-03-05 08:43:05 +0000

    Fixed code format issues in domains module.

--- src/domains.rs
+++ src/domains.rs
@@ -1,26 +1,23 @@
 #[derive(Serialize, Deserialize, Clone)]
-pub struct Item{
+pub struct Item {
     pub domain: String,
-    pub directive: Vec<String>
+    pub directive: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Collection {
-    pub domains: Vec<Item>
+    pub domains: Vec<Item>,
 }
 
 #[cfg(test)]
 mod item_test {
     #[test]
     fn test_item_struct() {
-        let directives: Vec<String> = vec![
-            String::from("connect-src"),
-            String::from("script-src")
-        ];
+        let directives: Vec<String> = vec![String::from("connect-src"), String::from("script-src")];
 
-        let item = super::Item{
+        let item = super::Item {
             domain: String::from("*.example.com"),
-            directive: directives
+            directive: directives,
         };
 
         assert_eq!(item.domain, "*.example.com");

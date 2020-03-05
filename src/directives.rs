713691f71783f35c96dae03c28a438393b856ac9
RobDWaller <rdwaller1984@googlemail.com>
2020-03-05 16:41:28 +0000

    Fixed failing directives tests after change to config module, added new test to capture failing state.

--- src/directives.rs
+++ src/directives.rs
@@ -6,16 +6,23 @@ use std::thread;
 use std::thread::JoinHandle;
 
 fn build_line(directive: String, domains: domains::Collection) -> String {
-    let mut directive_line: String = directive.to_string();
+    let mut directive_line: String = directive.clone();
+    let mut directive_check: String = directive.clone();
+    directive_check.push_str("; ");
 
     for domain in domains.domains {
-        if domain.directive.contains(&directive.to_string()) {
+        if domain.directive.contains(&directive) {
             directive_line.push_str(" ");
             directive_line.push_str(domain.domain.as_str());
         }
     }
 
     directive_line.push_str("; ");
+
+    if directive_line == directive_check {
+        return String::from("");
+    }
+
     directive_line
 }
 
@@ -27,7 +34,7 @@ fn build_lines(directives: Vec<String>, domains: domains::Collection) -> Vec<Joi
     let mut threads: Vec<JoinHandle<String>> = vec![];
 
     for directive in directives {
-        threads.push(self::create_thread(directive.to_string(), domains.clone()));
+        threads.push(self::create_thread(directive, domains.clone()));
     }
 
     threads
@@ -78,6 +85,27 @@ mod directives_test {
         let connect_src: String = super::build_line(String::from("connect-src"), json);
 
         assert_eq!(connect_src, String::from("connect-src *.example.com; "));
+    }
+
+    #[test]
+    fn test_build_line_no_directive() {
+        let directives: Vec<String> = vec![String::from("connect-src"), String::from("script-src")];
+
+        let item = domains::Item {
+            domain: String::from("*.example.com"),
+            directive: directives,
+        };
+
+        let mut domain_list: Vec<domains::Item> = Vec::new();
+        domain_list.push(item);
+
+        let json = domains::Collection {
+            domains: domain_list,
+        };
+
+        let default_src: String = super::build_line(String::from("default-src"), json);
+
+        assert_eq!(default_src, String::from(""));
     }
 
     #[test]

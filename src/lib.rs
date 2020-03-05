9b00f1071e038f72de510fd3579a68c0dc7790dc
RobDWaller <rdwaller1984@googlemail.com>
2020-03-05 08:40:21 +0000

    Fixed code format issues in lib module.

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@ use serde_json::error;
 #[macro_use]
 extern crate serde_derive;
 
+pub mod config;
+mod directives;
 mod domains;
 mod parse;
-mod directives;
-pub mod config;
 
 pub trait GetDirectives {
     fn get_directives(&self) -> Vec<String>;
@@ -14,26 +14,26 @@ pub trait GetDirectives {
 
 pub fn enforce(directives: impl GetDirectives, json: &str) -> String {
     let result: Result<String, error::Error> = directives::build(directives, json);
-    
+
     match result {
         Ok(result) => {
             let mut directive: String = String::from("content-security-policy ");
             directive.push_str(result.as_str());
             directive
-        },
-        Err(e) => panic!("Could not parse JSON: {}", e)
+        }
+        Err(e) => panic!("Could not parse JSON: {}", e),
     }
 }
 
 pub fn report_only(directives: impl GetDirectives, json: &str) -> String {
     let result: Result<String, error::Error> = directives::build(directives, json);
-    
+
     match result {
         Ok(result) => {
             let mut directive: String = String::from("content-security-policy-report-only ");
             directive.push_str(result.as_str());
             directive
-        },
-        Err(e) => panic!("Could not parse JSON: {}", e)
+        }
+        Err(e) => panic!("Could not parse JSON: {}", e),
     }
 }

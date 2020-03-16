ac16e9e0d600943663f63c66c00e29b3d7f2d545
RobDWaller <rdwaller1984@googlemail.com>
2020-03-16 14:18:44 +0000

    Added test for Collection struct to domains module.

--- src/domains.rs
+++ src/domains.rs
@@ -23,4 +23,22 @@ mod item_test {
         assert_eq!(item.domain, "*.example.com");
         assert_eq!(item.directive[1], "script-src");
     }
+
+    #[test]
+    fn test_collection() {
+        let directives: Vec<String> = vec![String::from("connect-src"), String::from("script-src")];
+
+        let item = super::Item {
+            domain: String::from("*.example.com"),
+            directive: directives,
+        };
+
+        let mut domains: Vec<super::Item> = vec![];
+        domains.push(item);
+
+        let collection = super::Collection{ domains };
+
+        assert_eq!(collection.domains[0].domain, "*.example.com");
+        assert_eq!(collection.domains[0].directive[1], "script-src");
+    }
 }

1147db500570123d24cf2f1ff33b5509ab632fb5
RobDWaller <rdwaller1984@googlemail.com>
2020-03-17 11:57:55 +0000

    In threads module renamed create thread method to create. Added test for create method.

--- src/directives/threads.rs
+++ src/directives/threads.rs
@@ -3,7 +3,7 @@ use crate::domains;
 use std::thread;
 use std::thread::JoinHandle;
 
-fn create_thread(directive: String, domains: domains::Collection) -> JoinHandle<String> {
+fn create(directive: String, domains: domains::Collection) -> JoinHandle<String> {
     thread::spawn(move || line::build(directive, domains.clone()))
 }
 
@@ -14,11 +14,32 @@ pub fn build_lines(
     let mut threads: Vec<JoinHandle<String>> = vec![];
 
     for directive in directives {
-        threads.push(self::create_thread(directive, domains.clone()));
+        threads.push(self::create(directive, domains.clone()));
     }
 
     threads
 }
 
 #[cfg(test)]
-mod threads_test {}
+mod threads_test {
+    use crate::domains;
+
+    #[test]
+    fn test_create() {
+        let domain = String::from("*.google.com");
+        let directive = vec![String::from("connect-src")];
+
+        let domain = domains::Item { domain, directive };
+        let domains = vec![domain];
+        let collection = domains::Collection { domains };
+
+        let directive_check = String::from("connect-src");
+
+        let result = super::create(directive_check, collection);
+
+        assert_eq!(
+            result.join().unwrap().as_str(),
+            "connect-src *.google.com; "
+        );
+    }
+}

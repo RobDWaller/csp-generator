72ec6b358402ab4393f8bdb89688d756b6328eb1
RobDWaller <rdwaller1984@googlemail.com>
2020-04-29 13:52:55 +0100

    Added ToJson trait to Collection so Items can be turned back into an JSON array easily. Added additional test and some tidy up.

--- src/domains.rs
+++ src/domains.rs
@@ -1,4 +1,7 @@
-#[derive(Serialize, Deserialize, Clone)]
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Item {
     pub domain: String,
     pub directives: Vec<String>,
@@ -6,16 +9,36 @@ pub struct Item {
 
 pub type Collection = Vec<Item>;
 
+pub trait ToJson {
+    fn to_json(&self) -> String;
+}
+
+impl ToJson for Collection {
+    fn to_json(&self) -> String {
+        let mut json = "[".to_string();
+
+        for item in self {
+            json.push_str(serde_json::to_string(&item).unwrap().as_str());
+        }
+
+        json.push_str("]");
+
+        json
+    }
+}
+
 // -----
 // Tests
 // -----
 #[cfg(test)]
 mod item_test {
+    use super::{Collection, Item, ToJson};
+
     #[test]
     fn test_item_struct() {
         let directives: Vec<String> = vec![String::from("connect-src"), String::from("script-src")];
 
-        let item = super::Item {
+        let item = Item {
             domain: String::from("*.example.com"),
             directives,
         };
@@ -28,15 +51,30 @@ mod item_test {
     fn test_collection() {
         let directives: Vec<String> = vec![String::from("connect-src"), String::from("script-src")];
 
+        let item = Item {
+            domain: String::from("*.example.com"),
+            directives,
+        };
+
+        let mut domains: Collection = Collection::new();
+        domains.push(item);
+
+        assert_eq!(domains[0].domain, "*.example.com");
+        assert_eq!(domains[0].directives[1], "script-src");
+    }
+
+    #[test]
+    fn test_to_json() {
+        let directives: Vec<String> = vec![String::from("connect-src"), String::from("script-src")];
+
         let item = super::Item {
             domain: String::from("*.example.com"),
             directives,
         };
 
-        let mut domains: super::Collection = vec![];
+        let mut domains: Collection = Collection::new();
         domains.push(item);
 
-        assert_eq!(domains[0].domain, "*.example.com");
-        assert_eq!(domains[0].directives[1], "script-src");
+        assert_eq!(domains.to_json(), r#"[{"domain":"*.example.com","directives":["connect-src","script-src"]}]"#);
     }
 }
